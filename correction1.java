fbf857b85f943891ebafb8647a9ae6ea9b64fa91
vanessahuhn <vanessa.huhn@gmail.com>
2018-07-10 11:14:25 +0200

    correction 1

---
+++ correction1.java
@@ -0,0 +1,51 @@
+/*correction exercices 1*/
+
+public class Name{
+    public static void main(String[]args){
+        /*var ex2*/
+        int nb1=74;
+        int nb2=36;
+        int nb3=5;
+        int nb4=10;
+        int nb5=15;
+        int nb6=10;
+        int sum=nb1+nb2+nb3+nb4+nb5+nb6;
+
+        /*var ex3*/
+        double nb7 = 50;
+        double nb8 = 3;
+        double div1 = nb7/nb8;
+
+        /*var ex4*/
+        int suma = -5 + 8 * 6;
+        int sumb = (55+9) % 9 ;
+        double sumc = 20 + -3*5 / 8 ;
+        double sumd = 5+15/3*2-8%3;
+
+        /*var ex5*/
+        int fstnb = 25;
+        int sdnb = 5;
+        int exout = fstnb *  sdnb;
+
+
+        /*met ex1*/
+        System.out.println("Hello");
+        System.out.println("Jean-Baptiste");
+
+        /*met ex2*/
+        System.out.println(sum);
+
+        /*met ex3*/
+        System.out.println(div1);
+
+        /*met ex4*/
+        System.out.println(suma);
+        System.out.println(sumb);
+        System.out.println(sumc);
+        System.out.println(sumd);
+
+        /*met ex5*/
+        System.out.println(fstnb + "*"+ sdnb + "=" + exout);
+    }
+
+}
