707be4ba2f51db53dfa799d19bf24aff44247f6c
vanessahuhn <vanessa.huhn@gmail.com>
2018-07-10 14:23:09 +0200

    correction 2

--- correction1.java
+++ correction1.java
@@ -49,3 +49,86 @@ public class Name{
     }
 
 }
+
+public class Exo{
+    public static void main(String[]args){
+
+        //exo 6
+        int fstnbr = 125;
+        int sdnbr  = 24;
+        int res = fstnbr + sdnbr;
+        String plus = "+";
+        String egale = "=";
+        System.out.println(fstnbr + plus + sdnbr + egale + res);
+
+        //exo 7
+        int fstnbr1 = 125;
+        int sdnbr2 = 24;
+        int div = fstnbr1 / sdnbr2;
+        System.out.println(fstnbr1 + " / " + sdnbr2 + " = " + div);
+
+        //exo 8
+        int fstnbr2 = 125;
+        int sdnbr3 = 24;
+        int mod = fstnbr2 % sdnbr3;
+        System.out.println(fstnbr2 + " mod " + sdnbr3 + " = " + mod);
+
+        //exo 9
+        int bb = 1;
+        while ( bb < 11){
+            System.out.println(8 + "*" + bb + "=" + (8 * bb) );
+            bb++;
+            }
+
+        //exo 10
+        double a=25.5;
+        double b=3.5;
+        double c=40.5;
+        double d=4.5;
+
+        double e=(a*b-b*b);
+        double f=(c-d);
+
+        System.out.println(e/f);
+
+        //exo 11
+        double result= 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(result);
+
+        //exo 12
+        double note1= 15.5;
+        double note2= 12.1;
+        double note3= 19.7;
+        double average= (note1 + note2 + note3)/3;
+        System.out.println(average);
+
+        //exo 13
+        int A = 1;
+        int B = 2;
+        int C = 0;
+        System.out.println("avant" + A + " " + B);
+        C = A;
+        A = B;
+        B = C;
+
+        System.out.println("après" + A + " " + B);
+
+
+    }
+
+}
+
+public class MyClass {
+   public static void main(String args[]) {
+       String verre1="tea";
+       String verre2="water";
+       String verreVide;
+
+       verreVide=verre1;
+       verre1=verre2;
+       verre2=verreVide;
+
+       System.out.println(verre1);
+       System.out.println(verre2);
+   }
+}
